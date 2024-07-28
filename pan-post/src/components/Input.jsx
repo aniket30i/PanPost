205d3b8852072210c417aadfd3ef270c4bd0fa7d
aniket30i <iamaniket.saha.09@protonmail.com>
2024-07-28 23:27:00 +0530

    prop

--- pan-post/src/components/Input.jsx
+++ pan-post/src/components/Input.jsx
@@ -8,11 +8,12 @@ const Input = ({
   fieldName,
   required,
   pattern,
-  onChange = () => {},
+  onChange = () => {}, // on operation
+  value = "",
 }) => {
   return (
     <div>
-      <div>
+      <div className="flex flex-col w-full">
         <p className="text-sm mx-2 mb-1">{fieldName}</p>
         <input
           type={type}
@@ -23,6 +24,7 @@ const Input = ({
           {...(pattern && { pattern })}
           onChange={onChange}
           className="rounded-xl p-1 px-4 w-4/5 border-2 border-zinc-300 self-start "
+          value={value}
         />
       </div>
     </div>
